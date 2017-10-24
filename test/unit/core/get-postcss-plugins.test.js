dff5a4769decab049eb1d7366feb3ccf95830ece
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2017-10-24 22:55:12 +0700

    Add support postcss plugins test

---
+++ test/unit/core/get-postcss-plugins.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const chai = require('chai')
+    , should = chai.should()
+    , utils = require('../utils')
+    , getPostcssPlugins = require('../../../src/core/get-postcss-plugins');
+
+describe('Test module src/core/get-postcss-plugins.js', function() {
+
+    it('Should return an empty array', function() {
+        let plugins = getPostcssPlugins(utils.defaultOptions);
+
+        plugins.should.to.be.an('array');
+        plugins.should.to.be.empty;
+    });
+
+    it('Should return a supported postcss plugins', function() {
+        utils.defaultOptions.autoprefixer = {
+            browsers: ['last 2 versions']
+        };
+        utils.defaultOptions.flexbugs_fixes = true;
+        utils.defaultOptions.oldie = true;
+
+        let plugins = getPostcssPlugins(utils.defaultOptions);
+
+        plugins.should.to.have.lengthOf(3);
+    });
+
+});
